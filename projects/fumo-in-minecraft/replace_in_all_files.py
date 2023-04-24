eaf833e3cc14f087746244dc66cee96c1c194da8
Starman-x64 <starman.x64@gmail.com>
2023-04-24 14:47:56 +0000

    Editing replace_in_all_files before mass replace

--- projects/fumo-in-minecraft/replace_in_all_files.py
+++ projects/fumo-in-minecraft/replace_in_all_files.py
@@ -1,13 +1,15 @@
 import os
 
 old_string = """<link rel="stylesheet" href="../../wiki-site-files/css/theme.css"></link>"""
-new_string = """<base href="./">\n<link rel="icon" type="image/x-icon" href="../../wiki-site-files/img/favicon.ico">\n<link rel="stylesheet" href="../../wiki-site-files/css/theme.css"></link>"""
+new_string = """<base href="./">\n    <link rel="icon" type="image/x-icon" href="../../wiki-site-files/img/favicon.ico">\n    <link rel="stylesheet" href="../../wiki-site-files/css/theme.css"></link>"""
 
 rootdir = os.getcwd() + "/projects/fumo-in-minecraft/"
 
+only_allowed_file_type = "index.html"
+
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
-        if not (".py" in file or ".zip" in file or ".png" in file or ".gif" in file or ".xlsx" in file):
+        if (not (".py" in file or ".zip" in file or ".png" in file or ".gif" in file or ".ico" in file or ".xlsx" in file)) and (only_allowed_file_type in file):
             print(subdir.split("/")[-1] + "/" + file)
             path = os.path.join(subdir, file)
             with open(path,"r+",encoding="utf8") as f:
